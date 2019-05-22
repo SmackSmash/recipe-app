3838c665e2f5ae79ae26c5ae9b192e74b510888a
Dan Moffat <danmoffat@gmail.com>
2019-05-22 15:36:28 +0100

    Added recipe list output logic

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'Gumbo',
+      'A spicy cajun dish!',
+      'https://spicysouthernkitchen.com/wp-content/uploads/seafood-gumbo-12.jpg'
+    ),
+    new Recipe(
+      'Bacon Sandwich',
+      'A British classic!',
+      'https://www.sickchirpse.com/wp-content/uploads/2017/08/Bacon-Sarnie.jpg'
+    )
+  ];
 
   constructor() {}
 
