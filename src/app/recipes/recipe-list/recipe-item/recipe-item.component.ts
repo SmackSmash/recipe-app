44b166a1d72c834d7e85e7ad7ff1c91186f08e9f
Dan Moffat <danmoffat@gmail.com>
2019-05-23 14:59:33 +0100

    Added my solution for displaying recipe details using data binding methods

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe;
+  @Output() recipeClicked = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  passRecipe(name: string) {
+    this.recipeClicked.emit(name);
   }
-
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -21,7 +21,13 @@ export class RecipeListComponent implements OnInit {
     )
   ];
 
+  @Output() showRecipe = new EventEmitter<Recipe[]>();
+
   constructor() {}
 
   ngOnInit() {}
+
+  passFullRecipe(name: string) {
+    this.showRecipe.emit(this.recipes.filter(recipe => recipe.name === name));
+  }
 }
