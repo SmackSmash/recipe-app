4dde956d9729c1f164925d4d2e179e6de58af2db
Dan Moffat <danmoffat@gmail.com>
2019-05-26 18:44:53 +0100

    Added my service based solution for the shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppingListService]
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [new Ingredient('Chicken Thighs', 6), new Ingredient('Onions', 1)];
+  ingredients: Ingredient[];
 
-  constructor() {}
+  constructor(private shoppingListService: ShoppingListService) {}
 
-  ngOnInit() {}
-
-  addIngredient(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+  ngOnInit() {
+    this.ingredients = this.shoppingListService.getIngredients();
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,1 +1,16 @@
-export class ShoppingListService {}
+import { Ingredient } from './../shared/ingredient.model';
+
+export class ShoppingListService {
+  private ingredients: Ingredient[] = [
+    new Ingredient('Chicken Thighs', 6),
+    new Ingredient('Onions', 1)
+  ];
+
+  getIngredients() {
+    return this.ingredients;
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+}
