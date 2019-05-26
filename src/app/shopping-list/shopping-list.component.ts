84a7dd1849162748f07435dacf6ba457ca0cb053
Dan Moffat <danmoffat@gmail.com>
2019-05-26 18:56:49 +0100

    Updated services method to always pass a copy of the ingredients array

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -6,8 +6,7 @@ import { ShoppingListService } from './shopping-list.service';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css'],
-  providers: [ShoppingListService]
+  styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[];
@@ -16,5 +15,8 @@ export class ShoppingListComponent implements OnInit {
 
   ngOnInit() {
     this.ingredients = this.shoppingListService.getIngredients();
+    this.shoppingListService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+      this.ingredients = ingredients;
+    });
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,16 +1,20 @@
 import { Ingredient } from './../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
 export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
   private ingredients: Ingredient[] = [
     new Ingredient('Chicken Thighs', 6),
     new Ingredient('Onions', 1)
   ];
 
   getIngredients() {
-    return this.ingredients;
+    return [...this.ingredients];
   }
 
   addIngredient(ingredient: Ingredient) {
     this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit([...this.ingredients]);
   }
 }
