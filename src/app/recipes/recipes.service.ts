a510fbaf91cca5199aba93599279bdbeb1907976
Dan Moffat <danmoffat@gmail.com>
2019-05-26 20:12:07 +0100

    Added ingredients list to recipe details

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from './../shared/ingredient.model';
 
 export class RecipesService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -9,12 +10,39 @@ export class RecipesService {
     new Recipe(
       'Gumbo',
       'A spicy cajun dish!',
-      'https://spicysouthernkitchen.com/wp-content/uploads/seafood-gumbo-12.jpg'
+      'https://spicysouthernkitchen.com/wp-content/uploads/seafood-gumbo-12.jpg',
+      [
+        new Ingredient('Skinless, Boneless, Chicken Thighs', 6),
+        new Ingredient('Jalapeno Peppers', 2),
+        new Ingredient('Andouille Sausage', 2),
+        new Ingredient('Onion', 1),
+        new Ingredient('Green Bell Pepper', 1),
+        new Ingredient('Celery', 2),
+        new Ingredient('Garlic', 6),
+        new Ingredient('Chicken Stock', 1),
+        new Ingredient('Thyme', 3),
+        new Ingredient('Vegetable Oil', 1),
+        new Ingredient('Flour', 1),
+        new Ingredient('Beer', 1),
+        new Ingredient('Salt', 1),
+        new Ingredient('Black Pepper', 1),
+        new Ingredient('Cayenne Pepper', 1),
+        new Ingredient('Bay Leaves', 3),
+        new Ingredient('Smoked Paprika', 1),
+        new Ingredient('Rice', 1),
+        new Ingredient('Spring Onion', 1)
+      ]
     ),
     new Recipe(
       'Bacon Sandwich',
       'A British classic!',
-      'https://www.sickchirpse.com/wp-content/uploads/2017/08/Bacon-Sarnie.jpg'
+      'https://www.sickchirpse.com/wp-content/uploads/2017/08/Bacon-Sarnie.jpg',
+      [
+        new Ingredient('White Bread', 1),
+        new Ingredient('Bacon', 6),
+        new Ingredient('Butter', 1),
+        new Ingredient('Brown Sauce', 1)
+      ]
     )
   ];
 
